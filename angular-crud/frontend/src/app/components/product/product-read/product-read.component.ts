d422a1eb90470bdf30c7e648475f28aad5c49151
ravanss <psaulo7@gmail.com>
2021-10-07 19:42:55 -0300

    alterando table, para receber funcionalide de edicação e delete dos elementos que estão no back-end

--- angular-crud/frontend/src/app/components/product/product-read/product-read.component.ts
+++ angular-crud/frontend/src/app/components/product/product-read/product-read.component.ts
@@ -10,8 +10,8 @@ import { ProductService } from '../product.service';
 export class ProductReadComponent implements OnInit {
 
   products!: Product[]
-  displayedColumns = ['id', 'name', 'price']
-  
+  displayedColumns = ['id', 'name', 'price', 'action']
+
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
